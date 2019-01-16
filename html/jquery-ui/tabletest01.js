eaf1e0fa66987d1b599d6213ae48b38f38574bdb
gensheng01 <genshengjiang@163.com>
2019-01-16 10:44:10 +0800

    上传新案例demo

---
+++ html/jquery-ui/tabletest01.js
@@ -0,0 +1,131 @@
+var tabletest = [
+	{
+		"id" : "1",
+		"title": "身份认证",
+		"children" : [
+			{
+				"type" : true,
+				"id" : "RF06007",
+				"content" : "申请人手机号码实名验证"
+			},
+			{
+				"type" : false,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : false,
+				"id" : "RF06023",
+				"content" : "申请人银行借记卡手机号四要素核验"
+			}
+		]
+	},
+	{
+		"id" : "2",
+		"title": "准入规则",
+		"children" : [
+			{
+				"type" : true,
+				"id" : "RF07001",
+				"content" : "年龄准入规则设置"
+			},
+			{
+				"type" : false,
+				"id" : "RF07010",
+				"content" : "手机号码归属地准入规则设置"
+			},
+			{
+				"type" : true,
+				"id" : "RF07014",
+				"content" : "手机号码入网时间准入规则设置"
+			}
+		]
+	},
+	{
+		"id" : "1",
+		"title": "身份认证",
+		"children" : [
+			{
+				"type" : true,
+				"id" : "RF06007",
+				"content" : "申请人手机号码实名验证申请人手机号码"
+			},
+			{
+				"type" : false,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : true,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : false,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : true,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : false,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : true,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : false,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : false,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : false,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : false,
+				"id" : "RF06015",
+				"content" : "申请人手机号码在网状态检验"
+			},
+			{
+				"type" : false,
+				"id" : "RF06023",
+				"content" : "申请人银行借记卡手机号四要素核验"
+			}
+		]
+	},
+	{
+		"id" : "2",
+		"title": "准入规则",
+		"children" : [
+			{
+				"type" : true,
+				"id" : "RF07001",
+				"content" : "年龄准入规则设置"
+			},
+			{
+				"type" : false,
+				"id" : "RF07010",
+				"content" : "手机号码归属地准入规则设置"
+			},
+			{
+				"type" : true,
+				"id" : "RF07014",
+				"content" : "手机号码入网时间准入规则设置"
+			}
+		]
+	}
+]
